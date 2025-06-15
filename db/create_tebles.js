b9564e525459067957957d8a581276044cd994cb
prince nyarko <prinako.dev+github@gmail.com>
2025-06-15 15:20:46 -0300

    on it

--- db/create_tebles.js
+++ db/create_tebles.js
@@ -4,36 +4,41 @@ const createTables = async () => {
     const db = await openDB()
     db.exec(`
         CREATE TABLE IF NOT EXISTS user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY CHECK (id = 1) NOT NULL UNIQUE DEFAULT 1,
             name TEXT,
             profile_pic TEXT
         );
     `)
-
-    db.exec(`
+    try {
+        db.exec(`
         CREATE TABLE IF NOT EXISTS score (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            points INTEGER NOT NULL,
-            level INTEGER NOT NULL,
+            id INTEGER PRIMARY KEY CHECK (id = 1) NOT NULL UNIQUE DEFAULT 1,
+            points INTEGER NOT NULL DEFAULT 0,
+            level INTEGER NOT NULL DEFAULT 1,
             user_id INTEGER NOT NULL,
             FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
         );
     `)
+    } catch (error) {
+        console.log(error)
+    }
+    
     db.exec(`
         CREATE TABLE IF NOT EXISTS aulas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
-            is_start INTEGER NOT NULL,
-            is_finish INTEGER NOT NULL,
+            is_start INTEGER NOT NULL DEFAULT 0,
+            is_finish INTEGER NOT NULL DEFAULT 0,
             step INTEGER NOT NULL
         );
     `)
     db.exec(`
         CREATE TABLE IF NOT EXISTS materials  (
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL UNIQUE,
-            link TEXT NOT NULL UNIQUE,
-            material INTEGER NOT NULL,
+            path TEXT NOT NULL UNIQUE,
+            is_question INTEGER NOT NULL DEFAULT 0,
+            is_answer INTEGER NOT NULL DEFAULT 0,            
             aula_id INTEGER NOT NULL,
             FOREIGN KEY(aula_id) REFERENCES aulas(id) ON DELETE CASCADE
         );
--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
 import createTables from "./db/create_tebles.js";
+import openDB from "./db/open_db.js";
+import { insertAulas, insertMaterials, insertScore } from "./db/insert_in_db.js";
+
+await createTables();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function  readJsonFile(file) {
+    const dataPath = path.join(__dirname, file);
+    const dataFiles = await readFile(dataPath, 'utf-8');
+    const data = await JSON.parse(dataFiles);
+
+    return data
+    
+}
+
+await insertScore();
+
+const db = await openDB();
+
+await insertAulas(db, await readJsonFile('./data/aulas.json'));
+await insertMaterials(db, await readJsonFile('./data/datilo.json'));
+await insertMaterials(db, await readJsonFile('./data/saudacao.json'));
+
+db.close();
 
 
-createTables();
+
